e23033a129fb6390e9f86e29c74fd4194b78732d
sai.akuleti <sai.akuleti@riktamtech.com>
2023-07-21 19:16:18 +0530

    new changes added check status endpoint

--- src/openapi.js
+++ src/openapi.js
@@ -6,121 +6,156 @@ const Results = require('./schemas/createFinalEvaluation');
 const TestSchema = require('./schemas/createTest');
 
 const configuration = new Configuration({
-    apiKey: process.env.OPENAI_WHISPER_API_KEY,
-  });
-  const openai = new OpenAIApi(configuration);
+  apiKey: process.env.OPENAI_WHISPER_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
 
 
-module.exports.convertAudioToText = async (req,res) => {
+module.exports.convertAudioToText = async (req, res) => {
   const { buffer, originalname } = req.file;
   const filePath = `/tmp/${originalname}`;
- try {
-  fs.writeFile(filePath, buffer, async (writeErr) => {
-    if (writeErr) {
-      // Handle file write error
-      return res.status(500).json({ error: 'Failed to write file.' });
-    }
-    const resp = await openai.createTranscription(
-      fs.createReadStream(filePath),
-      "whisper-1"
-    );
-    console.log('fileLocation',resp.data.text);
-    res.status(200).json({
+  try {
+    fs.writeFile(filePath, buffer, async (writeErr) => {
+      if (writeErr) {
+        // Handle file write error
+        return res.status(500).json({ error: 'Failed to write file.' });
+      }
+      const resp = await openai.createTranscription(
+        fs.createReadStream(filePath),
+        "whisper-1"
+      );
+      res.status(200).json({
         message: resp.data.text,
-        status:200
-    })
-  });
- } catch (error) {
-    console.log('eerrr',error);
+        status: 200
+      })
+    });
+  } catch (error) {
+    console.log('eerrr', error);
     res.status(500).json({
-        error:"error response",
-        status:500
+      error: "error response",
+      status: 500
     })
- }
+  }
 }
 
-module.exports.getCodeEditorStatus = async(req,res) => {
-  let {prompt} = req.body;
-   try {
-      const completion = await openai.createChatCompletion({
-        model: "gpt-3.5-turbo-0613",
-        messages: [
-        {role: "user", content: prompt}
-        ],
-        functions: [
-            {
-              name: "show_code_editor",
-              description: "User need to use code editor to provide there example",
-              parameters: {
-                type: "object",
-                properties: {
-                  show: {
-                    type: "boolean",
-                    description: "get the status"
-                  }
-                }
+module.exports.getCodeEditorStatus = async (req, res) => {
+  let { prompt } = req.body;
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo-0613",
+      messages: [
+        { role: "user", content: prompt }
+      ],
+      functions: [
+        {
+          name: "show_code_editor",
+          description: "User need to use code editor to provide there example",
+          parameters: {
+            type: "object",
+            properties: {
+              show: {
+                type: "boolean",
+                description: "get the status"
               }
             }
-          ]
-      });
-      let data = completion.data.choices[0];
-      if(data.finish_reason == "function_call"){
-        res.status(200).json({
-         isShowEditor : true
+          }
+        }
+      ]
+    });
+    let data = completion.data.choices[0];
+    if (data.finish_reason == "function_call") {
+      res.status(200).json({
+        isShowEditor: true
       })
-      }else{
-        res.status(200).json({
-          isShowEditor : false
-       })
-      }
-   } catch (error) {
+    } else {
+      res.status(200).json({
+        isShowEditor: false
+      })
+    }
+  } catch (error) {
     res.status(500).json({
-      error : error
-   })
-   }
+      error: error
+    })
+  }
 }
 
 
-module.exports.createChatCompletion = async (req,res) => {
-    let {systemPrompt,messages} = req.body;
-    let id = req.params.id;
-    try {
-        const completion = await openai.createChatCompletion({
-            model: "gpt-3.5-turbo",
-            messages: [
-                {"role": "system", "content": systemPrompt},
-                ...messages],
-          });
-         await setUserMessages(id,messages);
-        res.status(200).json({
-            message:completion.data.choices[0].message.content,
-            status:200
-        })
-     } catch (error) {
-        res.status(500).json({
-            error:"error fetching response",
-            status:500,
-            message:error
-        })
-     }
+
+const getCodeEditorStatuss = async (prompt) => {
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo-0613",
+      messages: [
+        { role: "user", content: prompt }
+      ],
+      functions: [
+        {
+          name: "show_code_editor",
+          description: "User need to use code editor to provide there example",
+          parameters: {
+            type: "object",
+            properties: {
+              show: {
+                type: "boolean",
+                description: "get the status"
+              }
+            }
+          }
+        }
+      ]
+    });
+    let data = completion.data.choices[0];
+    if (data.finish_reason == "function_call") {
+      return true;
+    } else {
+    }
+  } catch (error) {
+    return false;
+  }
 }
 
-module.exports.createFinalResults = async (req,res) => {
+module.exports.createChatCompletion = async (req, res) => {
+  let { systemPrompt, messages } = req.body;
+  let id = req.params.id;
+  try {
+    const completion = await openai.createChatCompletion({
+      model: "gpt-3.5-turbo",
+      messages: [
+        { "role": "system", "content": systemPrompt },
+        ...messages],
+    });
+    await setUserMessages(id, messages);
+    const prompt = completion.data.choices[0].message.content;
+    const codeStatus = await getCodeEditorStatuss(prompt);
+    res.status(200).json({
+      message: prompt,
+      status: 200,
+      editorStatus: codeStatus
+    })
+  } catch (error) {
+    res.status(500).json({
+      error: "error fetching response",
+      status: 500,
+      message: error
+    })
+  }
+}
+
+module.exports.createFinalResults = async (req, res) => {
   let data = req.body;
-  console.log('dataa',data);
   const db = new Results(data);
   try {
     const dataToSave = await db.save();
     res.status(200).json(dataToSave);
   } catch (error) {
     res.status(500).json({
-        error: error,
-        status:500
+      error: error,
+      status: 500
     })
   }
 }
 
-module.exports.createOnlineTest = async (req,res) => {
+module.exports.createOnlineTest = async (req, res) => {
   let data = req.body;
   const db = TestSchema(data)
   try {
@@ -129,21 +164,21 @@ module.exports.createOnlineTest = async (req,res) => {
   } catch (error) {
     res.status(500).json({
       error: error,
-      status:500
-  })
+      status: 500
+    })
   }
 }
 
-module.exports.updateEmailId = async (req,res) => {
+module.exports.updateEmailId = async (req, res) => {
   let id = req.params.id;
   let body = req.body;
   try {
     await TestSchema.findOneAndUpdate(
-      {_id: id},
+      { _id: id },
       {
-        $set: {userId : body.email}
-     },
-     {strict: false}
+        $set: { userId: body.email }
+      },
+      { strict: false }
     )
     const updatedUser = await TestSchema.findById(id);
     res.status(200).json(updatedUser)
@@ -154,27 +189,27 @@ module.exports.updateEmailId = async (req,res) => {
   }
 }
 
-const setUserMessages = async (id,messages) => {
-    try {
-      await TestSchema.findOneAndUpdate(
-        {_id: id},
-        {
-          $set: {questions : messages}
-       },
-       {strict: false}
-      )
-     return await TestSchema.findById(id);
-    } catch (error) {
-       return error;
-    }
+const setUserMessages = async (id, messages) => {
+  try {
+    await TestSchema.findOneAndUpdate(
+      { _id: id },
+      {
+        $set: { questions: messages }
+      },
+      { strict: false }
+    )
+    return await TestSchema.findById(id);
+  } catch (error) {
+    return error;
+  }
 }
 
 
-module.exports.updateUserMessages = async (req,res) => {
+module.exports.updateUserMessages = async (req, res) => {
   let id = req.params.id;
   let body = req.body;
   try {
-    const updatedUser = await setUserMessages(id,body.messages);
+    const updatedUser = await setUserMessages(id, body.messages);
     res.status(200).json(updatedUser)
   } catch (error) {
     res.status(500).json({
@@ -183,7 +218,7 @@ module.exports.updateUserMessages = async (req,res) => {
   }
 }
 
-module.exports.getPromptMessage = async (req,res) => {
+module.exports.getPromptMessage = async (req, res) => {
   let id = req.params.id;
   try {
     const data = await TestSchema.findById(id);
@@ -195,9 +230,9 @@ module.exports.getPromptMessage = async (req,res) => {
   }
 }
 
-module.exports.getAllUserInfo = async (req,res) => {
+module.exports.getAllUserInfo = async (req, res) => {
   try {
-    let data = await TestSchema.find({userId: { $exists: true }, questions: { $exists: true }});
+    let data = await TestSchema.find({ userId: { $exists: true }, questions: { $exists: true } });
     res.status(200).json(data);
   } catch (error) {
     res.status(500).json({
